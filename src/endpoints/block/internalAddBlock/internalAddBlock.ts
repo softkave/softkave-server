e24973efdb3f22d9460e46b4975b33645e6f46ab
Abayomi Akintomide <ywordk@gmail.com>
2020-09-07 06:09:18 -0400

    task resolutions

--- src/endpoints/block/internalAddBlock/internalAddBlock.ts
+++ src/endpoints/block/internalAddBlock/internalAddBlock.ts
@@ -4,66 +4,68 @@ import { BlockExistsError } from "../errors";
 import { InternalAddBlockEndpoint } from "./types";
 
 const internalAddBlock: InternalAddBlockEndpoint = async (
-  context,
-  instData
+    context,
+    instData
 ) => {
-  const user = await context.session.getUser(context, instData);
-  const inputBlock = instData.data.block;
+    const user = await context.session.getUser(context, instData);
+    const inputBlock = instData.data.block;
 
-  if (inputBlock.type !== "task") {
-    const blockExists = await context.blockExists(context, {
-      ...instData,
-      data: {
+    if (inputBlock.type !== BlockType.Task) {
+        const blockExists = await context.blockExists(context, {
+            ...instData,
+            data: {
+                name: inputBlock.name,
+                type: inputBlock.type,
+                parent: inputBlock.parent,
+            },
+        });
+
+        if (blockExists) {
+            // TODO: do a mapping of the correct error field name from called endpoints to the calling endpoints
+            // TODO: error field names should not be hardcoded
+            throw new BlockExistsError({
+                blockType: inputBlock.type,
+                field: "name",
+            });
+        }
+    }
+
+    const now = getDate();
+
+    const block: IBlock = {
+        customId: inputBlock.customId,
         name: inputBlock.name,
+        lowerCasedName:
+            inputBlock.name && inputBlock.type !== BlockType.Task
+                ? inputBlock.name.toLowerCase()
+                : undefined,
+        description: inputBlock.description,
+        dueAt: inputBlock.dueAt as any,
+        createdAt: now,
+        color: inputBlock.color,
+        updatedAt: undefined,
         type: inputBlock.type,
         parent: inputBlock.parent,
-      },
-    });
+        rootBlockId: inputBlock.rootBlockId,
+        createdBy: user.customId,
+        assignees: inputBlock.assignees,
+        priority: inputBlock.priority,
+        subTasks: inputBlock.subTasks,
+        boardLabels: inputBlock.boardLabels,
+        boardStatuses: inputBlock.boardStatuses,
+        boardResolutions: inputBlock.boardResolutions,
+        labels: inputBlock.labels,
+        status: inputBlock.status,
+        statusAssignedBy: inputBlock.statusAssignedBy,
+        statusAssignedAt: inputBlock.statusAssignedAt as any,
+        taskResolution: inputBlock.taskResolution,
+    };
 
-    if (blockExists) {
-      // TODO: do a mapping of the correct error field name from called endpoints to the calling endpoints
-      // TODO: error field names should not be hardcoded
-      throw new BlockExistsError({
-        blockType: inputBlock.type,
-        field: "name",
-      });
-    }
-  }
+    const savedBlock = await context.block.saveBlock(context, block);
 
-  const now = getDate();
-
-  const block: IBlock = {
-    customId: inputBlock.customId,
-    name: inputBlock.name,
-    lowerCasedName:
-      inputBlock.name && inputBlock.type !== BlockType.Task
-        ? inputBlock.name.toLowerCase()
-        : undefined,
-    description: inputBlock.description,
-    dueAt: inputBlock.dueAt as any,
-    createdAt: now,
-    color: inputBlock.color,
-    updatedAt: undefined,
-    type: inputBlock.type,
-    parent: inputBlock.parent,
-    rootBlockId: inputBlock.rootBlockId,
-    createdBy: user.customId,
-    assignees: inputBlock.assignees,
-    priority: inputBlock.priority,
-    subTasks: inputBlock.subTasks,
-    boardLabels: inputBlock.boardLabels,
-    boardStatuses: inputBlock.boardStatuses,
-    labels: inputBlock.labels,
-    status: inputBlock.status,
-    statusAssignedBy: inputBlock.statusAssignedBy,
-    statusAssignedAt: inputBlock.statusAssignedAt as any,
-  };
-
-  const savedBlock = await context.block.saveBlock(context, block);
-
-  return {
-    block: savedBlock,
-  };
+    return {
+        block: savedBlock,
+    };
 };
 
 export default internalAddBlock;
--- src/endpoints/block/types.ts
+++ src/endpoints/block/types.ts
@@ -1,54 +1,59 @@
 import {
-  BlockType,
-  IAssignee,
-  IBlockAssignedLabel,
-  IBlockLabel,
-  IBlockStatus,
-  ISubTask,
+    BlockType,
+    IAssignee,
+    IBlockAssignedLabel,
+    IBlockLabel,
+    IBlockStatus,
+    IBoardStatusResolution,
+    ISubTask,
 } from "../../mongo/block";
 
 export interface INewBlockInput {
-  // TODO: should we generate customId on our side, and have maybe something like clientGivenId
-  // with prefix c_id...?
-  customId: string;
-  type: BlockType;
-  name?: string;
-  description?: string;
-  dueAt?: string;
-  color?: string;
-  parent?: string;
-  rootBlockId?: string;
-  assignees?: IAssignee[];
-  priority?: string;
-  subTasks?: ISubTask[];
-  boardStatuses?: IBlockStatus[];
-  boardLabels?: IBlockLabel[];
-  status?: string;
-  statusAssignedBy?: string;
-  statusAssignedAt?: string;
-  labels?: IBlockAssignedLabel[];
+    // TODO: should we generate customId on our side, and have maybe something like clientGivenId
+    // with prefix c_id...?
+    customId: string;
+    type: BlockType;
+    name: string;
+    description?: string;
+    dueAt?: string;
+    color?: string;
+    parent?: string;
+    rootBlockId?: string;
+    assignees?: IAssignee[];
+    priority?: string;
+    subTasks?: ISubTask[];
+    boardStatuses?: IBlockStatus[];
+    boardLabels?: IBlockLabel[];
+    boardResolutions?: IBoardStatusResolution[];
+    status?: string;
+    statusAssignedBy?: string;
+    statusAssignedAt?: string;
+    taskResolution?: string;
+    labels?: IBlockAssignedLabel[];
 }
 
 export interface IPublicBlock {
-  customId: string;
-  createdBy: string;
-  createdAt: Date;
-  type: BlockType;
-  name?: string;
-  description?: string;
-  dueAt?: Date;
-  color?: string;
-  updatedAt?: Date;
-  updatedBy?: string;
-  parent?: string;
-  rootBlockId?: string;
-  assignees?: IAssignee[];
-  priority?: string;
-  subTasks?: ISubTask[]; // should sub-tasks be their own blocks?
-  boardStatuses?: IBlockStatus[];
-  boardLabels?: IBlockLabel[];
-  status?: string;
-  statusAssignedBy?: string;
-  statusAssignedAt?: Date;
-  labels?: IBlockAssignedLabel[];
+    customId: string;
+    createdBy: string;
+    createdAt: Date;
+    type: BlockType;
+    name: string;
+    description?: string;
+    dueAt?: Date;
+    color?: string;
+    updatedAt?: Date;
+    updatedBy?: string;
+    parent?: string;
+    rootBlockId?: string;
+    assignees?: IAssignee[];
+    priority?: string;
+    subTasks?: ISubTask[]; // should sub-tasks be their own blocks?
+    boardStatuses?: IBlockStatus[];
+    boardLabels?: IBlockLabel[];
+    boardResolutions?: IBoardStatusResolution[];
+    status?: string;
+    statusAssignedBy?: string;
+    statusAssignedAt?: Date;
+    taskResolution?: string;
+    labels?: IBlockAssignedLabel[];
 }
