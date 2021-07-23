fa67a6ee47c0a14e8b4f1038b5037e794f5c430a
Adepegba Adegoke <adepegbaadegoke@gmail.com>
2021-07-23 22:22:53 +0000

    Added two new test files

---
+++ src/endpoints/organization/__test__/getUserOrganization.test.ts
@@ -0,0 +1,34 @@
+import RequestData from "../../RequestData";
+import {
+    setupTestExpressRequest,
+    setupTestExpressRequestWithToken,
+} from "../../testUtils/setupTestExpressRequest";
+import { setupTestUser } from "../../testUtils/setupTestUser";
+import { getTestBaseContext } from "../../testUtils/TestBaseContext";
+import { wrapEndpoint } from "../../utils";
+import createOrganization from "../createOrganization/handler";
+import { ICreateOrganizationParameters } from "../createOrganization/types";
+import getUserOrganizations from "../getUserOrganizations/handler";
+
+const context = getTestBaseContext();
+
+const endpoint = (data, req) => {
+    return wrapEndpoint(data, req, async () =>
+        // @ts-ignore
+        getUserOrganizations(
+            context,
+            await RequestData.fromExpressRequest(context, req, data)
+        )
+    );
+};
+
+describe("get user organizations", () => {
+    test("can get organizations", async () => {
+        const { token } = await setupTestUser(context);
+        const { req } = setupTestExpressRequestWithToken({ token });
+        const result = await endpoint(undefined, req);
+        expect(result).toBeTruthy();
+        expect(result?.errors).toBeFalsy();
+        expect(result?.organizations).toBeTruthy();
+    });
+});
---
+++ src/endpoints/organization/__test__/organizationExists.test.ts
@@ -0,0 +1,36 @@
+import RequestData from "../../RequestData";
+import { setupTestExpressRequestWithToken } from "../../testUtils/setupTestExpressRequest";
+import { setupTestUser } from "../../testUtils/setupTestUser";
+import { getTestBaseContext } from "../../testUtils/TestBaseContext";
+import { wrapEndpoint } from "../../utils";
+import createOrganization from "../createOrganization/handler";
+import { ICreateOrganizationParameters } from "../createOrganization/types";
+import organizationExists from "../organizationExists/handler";
+import { IOrganizationExistsParameters } from "../organizationExists/types";
+
+const context = getTestBaseContext();
+
+const input: IOrganizationExistsParameters = {
+    name: "Test organization",
+};
+
+const endpoint = (data, req) => {
+    return wrapEndpoint(data, req, async () =>
+        // @ts-ignore
+        organizationExists(
+            context,
+            await RequestData.fromExpressRequest(context, req, data)
+        )
+    );
+};
+
+describe("organization exists", () => {
+    test("organization exists", async () => {
+        const { token } = await setupTestUser(context);
+        const { req } = setupTestExpressRequestWithToken({ token });
+        const result = await endpoint(input, req);
+        expect(result).toBeTruthy();
+        expect(result?.errors).toBeFalsy();
+        expect(result?.exists).toBeFalsy();
+    });
+});
