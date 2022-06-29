cadd731e45f492953210858f0cb098c052bc5656
Abayomi Akintomide <ywordk@gmail.com>
2022-06-28 21:56:50 -0400

    Fixes for build

--- src/endpoints/notifications/validation.ts
+++ src/endpoints/notifications/validation.ts
@@ -3,44 +3,35 @@ import { NotificationReason, NotificationType } from "../../mongo/notification";
 import { notificationConstants } from "./constants";
 
 const subscriptionResourceType = Joi.string().valid(
-    ...notificationConstants.subscriptionResourceTypes
+  ...notificationConstants.subscriptionResourceTypes
 );
 
 const reasonsArr: NotificationReason[] = [
-    NotificationReason.AddedByUser,
-    NotificationReason.UserCreatedResource,
-    NotificationReason.WasAssignedTask,
-    NotificationReason.WasAutoAssignedTask,
-    NotificationReason.UserIsResourceRecipient,
+  NotificationReason.AddedByUser,
+  NotificationReason.UserCreatedResource,
+  NotificationReason.WasAssignedTask,
+  NotificationReason.WasAutoAssignedTask,
+  NotificationReason.UserIsResourceRecipient,
 ];
 
 const notificationReason = Joi.string().valid(...reasonsArr);
 
 const typeArr: NotificationType[] = [
-    // NotificationType.OrganizationDeleted,
+  NotificationType.NewCollaborationRequest,
+  NotificationType.CollaborationRequestResponse,
+  NotificationType.CollaborationRequestUpdated,
+  NotificationType.CollaborationRequestRevoked,
 
-    NotificationType.CollaboratorRemoved,
-    NotificationType.CollaboratorPermissionsUpdated,
-    NotificationType.CollaboratorPermissionGroupsUpdated,
-
-    NotificationType.PermissionsUpdated,
-    NotificationType.ResourcePermissionGroupsUpdated,
-
-    NotificationType.NewCollaborationRequest,
-    NotificationType.CollaborationRequestResponse,
-    NotificationType.CollaborationRequestUpdated,
-    NotificationType.CollaborationRequestRevoked,
-
-    NotificationType.TaskAssigned,
-    NotificationType.TaskUnassigned,
+  NotificationType.TaskAssigned,
+  NotificationType.TaskUnassigned,
 ];
 
 const notificationType = Joi.string().valid(...typeArr);
 
 const notificationsValidationSchemas = {
-    notificationReason,
-    subscriptionResourceType,
-    notificationType,
+  notificationReason,
+  subscriptionResourceType,
+  notificationType,
 };
 
 export default notificationsValidationSchemas;
--- src/mongo/schemaVersions.ts
+++ src/mongo/schemaVersions.ts
@@ -1,11 +1,9 @@
 import { auditLogSchemaVersion } from "./audit-log";
 import { blockSchemaVersion } from "./block";
-import { notificationSchemaVersion } from "./notification";
 import { userSchemaVersion } from "./user";
 
 const schemaVersions = {
   block: blockSchemaVersion,
-  notfication: notificationSchemaVersion,
   user: userSchemaVersion,
   auditLog: auditLogSchemaVersion,
 };
